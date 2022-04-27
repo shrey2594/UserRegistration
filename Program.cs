be412a1b1a94835763698454ae79b2becd2bca2f
shrey2594 <shrey2510@gmail.com>
2022-04-28 02:14:58 +0530

    User Registration UC1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UserRegestration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first name");
+            string firstName=Console.ReadLine();
+            if(Validations.ValidateFirstName(firstName))
+            {
+                Console.WriteLine("First Name validated");
+            }
+            else
+            {
+                Console.WriteLine("First name invalid");
+            }
+        }
+    }
+
+}
---
+++ Validations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegestration
+{
+    internal class Validations
+    {
+        static string firstNamePattern = "^[A-Z]{1}[a-z]{3,}$";
+        
+        public static bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, firstNamePattern);
+        }
+    }
+}
